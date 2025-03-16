42f06416d3ad1dc7fa3204d7226b7df4c9a26338
Raj-yadav-7822 <ry682734@gmail.com>
2025-03-16 19:12:03 +0530

    Update app.js

--- NoteApp/app.js
+++ NoteApp/app.js
@@ -1,42 +1,4 @@
-// const addBtn = document.querySelector("#addBtn");
-// const main = document.querySelector("#main")
 
-// addBtn.addEventListener("click",function(){
-// addNote()
-// })
-
-// const saveNotes=()=>{
-//     const notes= document.querySelectorAll(".note textarea");
-//     console.log(notes);
-//     const data = [];
-//     notes.forEach(
-//         (note)=>{
-//         data.push(note.value)
-//     })
-//     localStorage.setItem("notes", JSON.stringify(data))
-// }
-
-
-// const addNote=()=>{
-//     const note = document.createElement('div');
-//     note.classList.add('note');
-//     note.innerHTML=`
-//     <div class="tool">
-//                 <i class="save fas fa-save"></i>
-//                 <i class="trash fas fa-trash"></i>
-//             </div>
-//             <textarea></textarea>
-    
-//     `
-//     note.querySelector('.trash').addEventListener("click",function(){
-//         note.remove()
-//     })
-//     note.querySelector('.save').addEventListener("click",function(){
-//         saveNotes()
-//     })
-//     main.appendChild(note)
-//     saveNotes()
-// }
 
 
 
